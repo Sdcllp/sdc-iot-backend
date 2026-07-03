d0bd8b9dee5f50900c264939a2c6e5e34690ab8a
Sdcllp <201740252+Sdcllp@users.noreply.github.com>
2026-07-03 15:07:49 +0530

    changes in routes

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -5,23 +5,32 @@ import {
   controlLight,
   controlAC,
   controlACTemp,
-  controlPanasonicAC
+  controlPanasonicAC,
 } from "../controllers/deviceController.js";
 
 const router = express.Router();
 
+/* Test route */
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Device routes working",
+  });
+});
+
+/* Get latest device data */
 router.get("/", getDeviceData);
 
+/* Light control */
 router.post("/light", controlLight);
 
+/* Mitsubishi AC control */
 router.post("/ac", controlAC);
 
+/* Mitsubishi AC temperature control */
 router.post("/ac-temp", controlACTemp);
 
-// Panasonic AC route
-router.post(
-"/panasonic-ac",
-controlPanasonicAC
-);
+/* Panasonic AC control */
+router.post("/panasonic-ac", controlPanasonicAC);
 
 export default router;
